8213867c0a31304613e31752f0bb386bcdfed10d
Chris <christopher_obieke@outlook.com>
2024-03-13 18:58:52 -0400

    urdf file uploaded and running

---
+++ build/hitc_robot_description/CMakeFiles/hitc_robot_description_uninstall.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for custom commands dependencies management for hitc_robot_description_uninstall.
--- build/path_planner/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/chris/capstone/hitc_ws/install/path_planner'
